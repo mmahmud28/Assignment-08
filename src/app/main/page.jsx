87b9dc392bdc8fc32242e31685a7bcfb1da187be
mahmudpython <mdmhamud3311@gmail.com>
2026-05-09 21:51:24 +0600

    Add Deploy Link

--- src/app/main/page.jsx
+++ src/app/main/page.jsx
@@ -1,11 +1,22 @@
+"use client";
 import React from 'react';
 import Banner from '../components/home/Banner';
 import Marquee from '../components/home/Marquee';
 import FeaturedTiles from '../components/home/FeaturedTiles';
 import Footer from '../components/shared/Footer';
+import { authClient } from '@/lib/auth-client';
+import { reddit } from 'better-auth';
 
 
 const Mainpage = () => {
+
+      const userData = authClient.useSession();
+      const userdata = userData?.data?.user;
+
+      if(!userdata){
+        reddit:('/')
+      }
+
     return (
         <div>
            <Banner/>
